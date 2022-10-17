1a680a20c1babec4fb4b331f774d019d93a21444
Znlou <93763574+Znlou@users.noreply.github.com>
2022-10-18 02:23:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ nowtime = datetime.utcnow() + timedelta(hours=8)  # 东八区时间
 today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d") #今天的日期
 
 start_date = os.getenv('START_DATE')
-city = os.getenv('city')
+city = os.getenv('CITY')
 birthday = os.getenv('BIRTHDAY')
 
 app_id = os.getenv('APP_ID')
